b59c29ead5b46f6ec08f7dacd0f1b749de8e09c0
Johann Og <kronolynx@gmail.com>
2016-03-24 20:02:43 +0100

    Add login form

--- KronoBlog/Controllers/AuthController.cs
+++ KronoBlog/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KronoBlog.ViewModels;  // important to use our ViewModels 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,26 @@ namespace KronoBlog.Controllers
 {
     public class AuthController : Controller
     {
+        [HttpGet]
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(form);
+            }
+            if (form.Username.Length < 5)
+            {
+                // we can use our own validation and generate our own errors
+                ModelState.AddModelError("Username", "Username must be longer than 5 characters");
+                return View(form);
+            }
+            return Content("hello " + form.Username);
         }
     }
 }
