b7271c0865b7d9cc1308374aa42a81035a996189
Johann Og <kronolynx@gmail.com>
2016-03-24 14:18:06 +0100

    Add basic routes, controllers, admin area

---
+++ KronoBlog/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KronoBlog.Areas.Admin.Controllers
+{
+    public class UsersController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("Users");
+        }
+    }
+}
