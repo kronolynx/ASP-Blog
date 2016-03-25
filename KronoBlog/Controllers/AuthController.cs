06218823f550e5c8a461e8a6b4e5ff60928c9486
Johann Og <kronolynx@gmail.com>
2016-03-25 13:18:33 +0100

    Implement authentication and custom role provider

--- KronoBlog/Controllers/AuthController.cs
+++ KronoBlog/Controllers/AuthController.cs
@@ -4,11 +4,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace KronoBlog.Controllers
 {
     public class AuthController : Controller
     {
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            // here should go a flash message
+            return RedirectToRoute("home");
+        }
         [HttpGet]
         public ActionResult Login()
         {
@@ -16,19 +23,21 @@ namespace KronoBlog.Controllers
         }
 
         [HttpPost]
-        public ActionResult Login(AuthLogin form)
+        public ActionResult Login(AuthLogin form, string returnUrl)
         {
             if (!ModelState.IsValid)
             {
                 return View(form);
             }
-            if (form.Username.Length < 5)
-            {
-                // we can use our own validation and generate our own errors
-                ModelState.AddModelError("Username", "Username must be longer than 5 characters");
-                return View(form);
-            }
-            return Content("hello " + form.Username);
+            // true is for CreatePersistenCookie
+            FormsAuthentication.SetAuthCookie(form.Username, true);
+
+            // check if we have a return url and redirect if needed
+            if (!string.IsNullOrWhiteSpace(returnUrl))
+                return Redirect(returnUrl);
+
+            // redirect to home
+            return RedirectToRoute("home");
         }
     }
 }
