df308e1887670c4e737584df67c8ba5354ac6fd0
Johann Og <kronolynx@gmail.com>
2016-03-25 17:00:36 +0100

    Add styles, scripts and BundleConfig

---
+++ KronoBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace KronoBlog.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // admin bundle style
+            bundles.Add(new StyleBundle("~/admin/styles").
+                Include("~/content/styles/bootstrap.css").
+                Include("~/content/style/admin.css"));
+
+            // site bundle style
+            bundles.Add(new StyleBundle("~/styles").
+                Include("~/content/styles/bootstrap.css").
+                Include("~/content/style/site.css"));
+           
+            // admin scripts
+            bundles.Add(new ScriptBundle("~/admin/scripts").
+                Include("~/scripts/jquery-2.2.1.js").
+                Include("~/scripts/jquery.validate.js").
+                Include("~/scripts/jquery.validate.unobtrusive.js").
+                Include("~/scripts/bootstrap.js"));
+
+            // site scripts
+            bundles.Add(new ScriptBundle("~/scripts").
+                Include("~/scripts/jquery-2.2.1.js").
+                Include("~/scripts/jquery.validate.js").
+                Include("~/scripts/jquery.validate.unobtrusive.js").
+                Include("~/scripts/bootstrap.js"));
+        }
+    }
+}
--- KronoBlog/Global.asax.cs
+++ KronoBlog/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using KronoBlog.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace KronoBlog
@@ -13,6 +15,7 @@ namespace KronoBlog
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
